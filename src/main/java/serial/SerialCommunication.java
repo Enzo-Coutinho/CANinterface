334e2b684666421cee0e7f7e07e7601d9fc7ed31
Enzo-Coutinho <enzo@stemos.com.br>
2026-01-16 23:03:48 -0300

    create first function to interact with serial comm

--- src/main/java/serial/SerialCommunication.java
+++ src/main/java/serial/SerialCommunication.java
@@ -1,11 +1,35 @@
 package serial;
+import com.fazecast.jSerialComm.*;
 
 public class SerialCommunication {
 
     final int BAUDRATE;
+    final SerialPort serialPort;
 
-    SerialCommunication(int BAUDRATE) {
+    SerialCommunication(int BAUDRATE, String comPort) {
         this.BAUDRATE = BAUDRATE;
+        serialPort = SerialPort.getCommPort(comPort);
     }
 
+    public void start() {
+        serialPort.openPort();
+        setModeToReadBlocking();
+    }
+
+    public void close() {
+        serialPort.closePort();
+    }
+
+    public int receiveMessage() {
+        byte[] readBuffer = new byte[1024];
+        return serialPort.readBytes(readBuffer, readBuffer.length);
+    }
+
+    private void setModeToReadBlocking() {
+        setPortMode(SerialPort.TIMEOUT_READ_BLOCKING);
+    }
+
+    private void setPortMode(final int mode) {
+        serialPort.setComPortTimeouts(mode, 1000, 0);
+    }
 }
