d9c452300a70ec0a82ca0950fb184bb1b316d3cd
Enzo-Coutinho <enzo@stemos.com.br>
2026-01-20 17:55:25 -0300

    add more variables and functions (not work yet)

--- src/main/java/CANBus/CANMessageFormatter.java
+++ src/main/java/CANBus/CANMessageFormatter.java
@@ -2,13 +2,32 @@ package CANBus;
 
 public class CANMessageFormatter {
 
+    final int timestamp;
+
     final DeviceTypes devicesTypes;
     final Manufacturer manufacturer;
+    final boolean isRemoteFrame;
+    final boolean isErrorFrame;
+    final boolean b;
+    final boolean iErrorFrame;
+    final int data;
 
     public CANMessageFormatter(final int message) {
-        devicesTypes = decodeDevicesTypes(message);
-        manufacturer = decodeManufacturer(message);
+        this.timestamp = getTimestampFromFullMessage(message);
+        final int header = getHeaderFromFullMessage(message);
+        devicesTypes = decodeDevicesTypes(header);
+        manufacturer = decodeManufacturer(header);
     }
+
+    private int getHeaderFromFullMessage(final int message) {
+        return 0;
+    }
+
+    private int getTimestampFromFullMessage(final int message) {
+        return 0;
+    }
+
+
 
     private DeviceTypes decodeDevicesTypes(final int message) {
         return DeviceTypes.getFromIdentification((message >> 24) & DeviceTypes.BIT_MASK);
